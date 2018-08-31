59c785729d18bcde75c0f8ec071ed2b198891b2d
huangfuhui <huangfuhui@meimeifa.com>
2018-08-31 10:58:29 +0800

    修改逻辑

--- env/env_robot.go
+++ env/env_robot.go
@@ -10,14 +10,14 @@ import (
 func main() {
     defer func() {
             if err := recover(); err != nil {
-                // fmt.Println(err)
+                fmt.Println(err)
                 fmt.Println("发生了一些未知错误...")
             }
            }()
 
     argsQuantity := len(os.Args)
-    if argsQuantity < 3 {
-        fmt.Println("请输入以下命令参数 env[dev sit uat deploy] env_path")
+    if argsQuantity < 4 {
+        fmt.Println("请输入以下命令参数 env[dev sit uat deploy] new_env_path old_env_path")
         return
     }
 
@@ -26,11 +26,18 @@ func main() {
         fmt.Println("请选择正确的环境值 -> [dev sit uat deploy]")
         return
     }
-    envPath := os.Args[2]
-    if envPath == "" {
-        envPath = "./"
-    } else if !isFileExist(envPath) {
-        fmt.Println("文件夹 " + envPath + " 不存在")
+    newEnvPath := os.Args[2]
+    if newEnvPath == "" {
+        newEnvPath = "./"
+    } else if !isFileExist(newEnvPath) {
+        fmt.Println("文件夹 " + newEnvPath + " 不存在")
+        return
+    }
+    oldEnvPath := os.Args[3]
+    if oldEnvPath == "" {
+        oldEnvPath = "./"
+    } else if !isFileExist(oldEnvPath) {
+        fmt.Println("文件夹 " + oldEnvPath + " 不存在")
         return
     }
 
@@ -39,36 +46,36 @@ func main() {
     var err error
     switch strings.TrimSpace(env) {
         case "dev":
-            if !isFileExist("dev.env") {
-                fmt.Println("文件 ./dev.env 不存在")
+            if !isFileExist(newEnvPath + "/dev.env") {
+                fmt.Println("文件 " + newEnvPath + "/dev.env 不存在")
                 return
             }
-            config, err = ioutil.ReadFile("dev.env")
+            config, err = ioutil.ReadFile(newEnvPath + "/dev.env")
         case "sit":
-            if !isFileExist("sit.env") {
-                fmt.Println("文件 ./sit.env 不存在")
+            if !isFileExist(newEnvPath + "/sit.env") {
+                fmt.Println("文件 " + newEnvPath + "/sit.env 不存在")
                 return
             }
-            config, err = ioutil.ReadFile("sit.env")
+            config, err = ioutil.ReadFile(newEnvPath + "/sit.env")
         case "uat":
-            if !isFileExist("uat.env") {
-                fmt.Println("文件 ./uat.env 不存在")
+            if !isFileExist(newEnvPath + "/uat.env") {
+                fmt.Println("文件 " + newEnvPath + "/uat.env 不存在")
                 return
             }
-            config, err = ioutil.ReadFile("uat.env")
+            config, err = ioutil.ReadFile(newEnvPath + "/uat.env")
         case "deploy":
-            if !isFileExist("deploy.env") {
-                fmt.Println("文件 ./deploy.env 不存在")
+            if !isFileExist(newEnvPath + "/deploy.env") {
+                fmt.Println("文件 " + newEnvPath + "/deploy.env 不存在")
                 return
             }
-            config, err = ioutil.ReadFile("deploy.env")
+            config, err = ioutil.ReadFile(newEnvPath + "/deploy.env")
     }
     if err != nil {
         panic(err)
     }
 
     // 覆盖已有配置
-    err = ioutil.WriteFile(envPath + "/.env", config, 0644)
+    err = ioutil.WriteFile(oldEnvPath + "/.env", config, 0644)
     if err != nil {
         panic(err)
     }
